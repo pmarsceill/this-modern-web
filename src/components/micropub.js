a2829f96a8a7320cc15f517c22cb727a10c07861
Patrick Marsceill <patrick.marsceill@gmail.com>
2019-09-30 22:50:03 -0400

    micropub

---
+++ src/components/micropub.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+
+function Micropub() {
+  return (
+    <Helmet>
+      <link rel="authorization_endpoint" href="https://indieauth.com/auth" />
+      <link rel="token_endpoint" href="https://tokens.indieauth.com/token" />
+      <link rel="micropub" href="https://tmw-mp-enpoint.glitch.me/" />
+    </Helmet>
+  )
+}
+
+export default Micropub
