299e6269b62e2feff4e54f6c2efc8e84c2b12162
Patrick Marsceill <pmarsceill@users.noreply.github.com>
2020-03-03 13:08:35 -0500

    Add analytics tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,7 +99,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-2709176-11`,
       },
     },
     {
