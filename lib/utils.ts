c27ba3fa024d47da0d544b2d7cb69135bed01237
Patrick Marsceill <pmarsceill@users.noreply.github.com>
2022-01-15 21:00:08 +0000

    add utils.ts and fix microblog

---
+++ lib/utils.ts
@@ -0,0 +1,22 @@
+// NOTE The functionality below (incl. TS types) will soon become part of Contentlayer itself. Please don't mind its existence. 😎
+
+type ConvertUndefined<T> = OrNull<{
+  [K in keyof T as undefined extends T[K] ? K : never]-?: T[K]
+}>
+type OrNull<T> = { [K in keyof T]: Exclude<T[K], undefined> | null }
+type PickRequired<T> = {
+  [K in keyof T as undefined extends T[K] ? never : K]: T[K]
+}
+type ConvertPick<T> = ConvertUndefined<T> & PickRequired<T>
+
+const pick = <Obj, Keys extends keyof Obj>(
+  obj: Obj,
+  keys: Keys[]
+): ConvertPick<{ [K in Keys]: Obj[K] }> => {
+  return keys.reduce((acc, key) => {
+    acc[key] = obj[key] ?? null
+    return acc
+  }, {} as any)
+}
+
+export { pick }
